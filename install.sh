be532e4b3f76fcdfe20eae516b2d9aa28739328e
gerardPlanella <gerard.plfo@gmail.com>
2023-01-11 14:34:45 +0100

    changed install script echos

--- install.sh
+++ install.sh
@@ -9,26 +9,26 @@ done
 
 
 git clone --recurse-submodules https://github.com/gerardPlanella/multiagent_fairness_reproducibility.git
-echo "Entering Repo..."
+echo "----- Entering Repo -----"
 cd multiagent_fairness_reproducibility
-echo "Creating Conda Environment.."
+echo "----- Creating Conda Environment -----"
 conda create -n "fact22" python=3.5.2
-echo "Activating Conda Environment"
+echo "----- Activating Conda Environment -----"
 conda activate fact22
-echo "Installing PyTorch"
+echo "----- Installing PyTorch -----"
 pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org torch===1.5.0 torchvision===0.6.0 -f https://download.pytorch.org/whl/torch_stable.html
-echo "Installing Remaining Libraries..."
+echo "----- Installing Remaining Libraries -----"
 pip install -r requirements_v2.txt
 
 if [[ $OSTYPE == 'darwin'* ]]; then
-  echo 'macOS detected, installing specific libraries...'
+  echo '----- macOS detected, installing specific libraries -----'
   pip install pyglet==1.5.11
 fi
 
 cd simple_particle_envs
-echo "Installing Simple Particle Environment..."
+echo "----- Installing Simple Particle Environment -----"
 pip install -e .
 cd ..
-echo "Deactivating conda environment"
+echo "----- Deactivating conda environment -----"
 conda deactivate
-echo "Installation finished..."
+echo "-----Installation finished -----"
